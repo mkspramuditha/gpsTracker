a0501f248f7150c130a265a0d0856d54e165479a
mkspramuditha <mkspramuditha@gmail.com>
2017-01-16 12:46:12 +0530

    tcpServer created

---
+++ tcpServer.js
@@ -0,0 +1,27 @@
+var net = require('net');
+var validator = require('./validator.js');
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+validator.validate("shan");
+
+net.createServer(function(sock) {
+
+
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    sock.on('data', function(data) {
+
+        console.log(data);
+
+    });
+
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+
---
+++ validator.js
@@ -0,0 +1,12 @@
+module.exports.validator = function(mm,r){
+
+    this.deviceList = [];
+    this.unknownDeviceList = [];
+
+    this.validate = function (message) {
+        console.log(message);
+    }
+
+
+};
+
