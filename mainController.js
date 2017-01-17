abf61e22bd0d364d0d554afd08fbd215778ef8ad
mkspramuditha <mkspramuditha@gmail.com>
2017-01-17 23:48:14 +0530

    addto redis created function

--- mainController.js
+++ mainController.js
@@ -1,20 +1,23 @@
 module.exports.controller = function() {
 
     var redis = require('redis');
-    // var client = redis.createClient(6379,'127.0.0.1');
+    var client = redis.createClient(6379,'127.0.0.1');
     this.data = "";
 
     var wss = require('./wsServer');
+    var store = require('./temporaryStore.js');
+    store = new store.temporaryStore();
 
     wss = new wss.wsServer();
     
     this.send = function(message){
-        temporaryStore(message);
+        temporaryStore(message,client);
         sendWs(message);
     }
 
-    function temporaryStore(message) {
+    function temporaryStore(message,client) {
         // client.set("dsd", message);
+        store.store(message,client);
     };
     
 
--- temporyStore.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by shan on 1/17/17.
- */
