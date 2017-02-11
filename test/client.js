c14ae08d0eddfa2ed97809af36bef33823a9dd38
mkdpriyankara@gmail.com <mkdpriyankara@gmail.com>
2017-02-11 17:04:49 +0530

    finalizing
    add example messages for 16 and 13

--- test/client.js
+++ test/client.js
@@ -5,6 +5,8 @@ client.connect(4444, '127.0.0.1', function() {
 
     console.log('Connected');
     var value12 = '78781f12100a100d1408c70211111100322222003401019d01cb20002e02000db1b20d0a';
+    var value16 = '787825160B0B0F0E241DCF027AC8870C4657E60014020901CC00287D001F726506040101003656A40D0A';
+    var value13 = '787808134B040300010011061F0D0A';
     var value1 = '78780d0103554880201067640004b1bb0d0a';
     client.write(new Buffer(value1, 'hex'),function () {
         client.write(new Buffer(value12, 'hex'),function () {
